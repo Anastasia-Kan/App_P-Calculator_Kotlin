adfd993d1f0befabb2aca2e3fe6160be834b6c0e
anastasia.kantor <anastasia.kantor@gmail.com>
2021-01-14 15:46:15 +0100

    a lot of mess

--- app/src/main/java/com/example/pressurecalctest1/MainActivity.kt
+++ app/src/main/java/com/example/pressurecalctest1/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.pressurecalctest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatRadioButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val shen = findViewById<AppCompatRadioButton>(R.id.shen_segment)
+        val mao_hydro = findViewById<AppCompatRadioButton>(R.id.mao_hydro_segment)
+        val mao_nHydro = findViewById<AppCompatRadioButton>(R.id.mao_nHydro_segment)
+
     }
+
+    fun onSegmentClicked(view: View) {
+        var isSelected = false;
+        switch (view.getId()) {
+            case shen
+        }
+    }
+
+    private fun AppCompatRadioButton(): Any {
+
+    }
+/*
+    public void onSegmentClicked(View view) {
+        var isSelected = ((androidx.appcompat.widget.AppCompatRadioButton())view).isChecked();
+        switch (view.getId()) { case R . layout . id . shen_segment : }
+    }*/
 }

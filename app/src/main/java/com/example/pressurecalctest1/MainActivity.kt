839d7822608ca7373fb47622a8c886e15ac70cbe
anastasia.kantor <anastasia.kantor@gmail.com>
2021-01-27 11:45:56 +0100

    Tabs wrapped in Fragments

--- app/src/main/java/com/example/pressurecalctest1/MainActivity.kt
+++ app/src/main/java/com/example/pressurecalctest1/MainActivity.kt
@@ -2,28 +2,32 @@ package com.example.pressurecalctest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatRadioButton
 
 class MainActivity : AppCompatActivity() {
+
+    val rubyView = RubyFragment()
+    val diamondView = DiamondFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val shen = findViewById<AppCompatRadioButton>(R.id.shen_segment)
-        val mao_hydro = findViewById<AppCompatRadioButton>(R.id.mao_hydro_segment)
-        val mao_nHydro = findViewById<AppCompatRadioButton>(R.id.mao_nHydro_segment)
-// Add radioGroup.setOnCheckedChangeListener and button.setOnClickListener:
-// see geeksforgeeks.org/radiobutton-in-kotlin -> on Dell PC
-    }
 
-    fun onSegmentClicked(view: View) {
-        if (view is AppCompatRadioButton) {
-            val checked = view.isChecked
-            when (view.getId()) {
-                R.id.shen_segment -> if (checked) {println("Shen chosen")}
-                R.id.mao_hydro_segment -> if (checked) {println("Mao hydro chosen")}
-                R.id.mao_nHydro_segment -> if (checked) {println("Mao non-hydro chosen")}
-            }
+        supportFragmentManager.beginTransaction().add(R.id.mainFragmentContainer, rubyView).commit()
+
+        val rubyTab = findViewById<Button>(R.id.rubyTab)
+        val diaTab = findViewById<Button>(R.id.diamondTab)
+
+        rubyTab.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.mainFragmentContainer, rubyView).commit()
         }
+        diaTab.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.mainFragmentContainer, diamondView).commit()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/pressurecalctest1/RubyFragment.kt
@@ -0,0 +1,44 @@
+package com.example.pressurecalctest1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatRadioButton
+
+class RubyFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+// Add radioGroup.setOnCheckedChangeListener and button.setOnClickListener:
+// see geeksforgeeks.org/radiobutton-in-kotlin -> on Dell PC
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+      // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ruby, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val shen = view.findViewById<AppCompatRadioButton>(R.id.shen_segment)
+        val mao_hydro = view.findViewById<AppCompatRadioButton>(R.id.mao_hydro_segment)
+        val mao_nHydro = view.findViewById<AppCompatRadioButton>(R.id.mao_nHydro_segment)
+    }
+
+    fun onSegmentClicked(view: View) {
+        if (view is AppCompatRadioButton) {
+            val checked = view.isChecked
+            when (view.getId()) {
+                R.id.shen_segment -> if (checked) {println("Shen chosen")}
+                R.id.mao_hydro_segment -> if (checked) {println("Mao hydro chosen")}
+                R.id.mao_nHydro_segment -> if (checked) {println("Mao non-hydro chosen")}
+            }
+        }
+    }
+}

b66a094986a310b2ee5387205ceb6f79e7eebd8e
anastasia.kantor <anastasia.kantor@gmail.com>
2021-01-27 13:28:56 +0100

    dependency added

--- app/src/main/java/com/example/pressurecalcapp/RubyFragment.kt
+++ app/src/main/java/com/example/pressurecalcapp/RubyFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import androidx.appcompat.widget.AppCompatRadioButton
 
 class RubyFragment : Fragment() {

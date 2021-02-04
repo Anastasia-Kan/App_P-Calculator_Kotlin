0c818f49949291943c9361932d0c7e4f71130dce
anastasia.kantor <anastasia.kantor@gmail.com>
2021-02-04 15:25:15 +0100

    Ruby fragment - all elements added

--- app/src/main/java/com/example/pressurecalcapp/RubyFragment.kt
+++ app/src/main/java/com/example/pressurecalcapp/RubyFragment.kt
@@ -1,6 +1,9 @@
 package com.example.pressurecalcapp
 
+import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,70 +11,35 @@ import android.view.ViewGroup
 import android.widget.RadioGroup
 import androidx.appcompat.widget.AppCompatRadioButton
 
-class RubyFragment : Fragment() {
+class RubyFragment : Fragment(), View.OnClickListener {
+
+    val TAG = "pia9debug"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-         val view = inflater.inflate(R.layout.fragment_ruby, container, false)
-
-        /*fun onSegmentClicked(view: View) {
-            if (view is AppCompatRadioButton) {
-                val checked = view.isChecked
-                when (view.getId()) {
-                    R.id.shen_segment -> if (checked) {
-                        println("Shen chosen")
-                    }
-                    R.id.mao_hydro_segment -> if (checked) {
-                        println("Mao hydro chosen")
-                    }
-                    R.id.mao_nHydro_segment -> if (checked) {
-                        println("Mao non-hydro chosen")
-                    }
-                }
-            }
-
-        }*/
-
+        val view = inflater.inflate(R.layout.fragment_ruby, container, false)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val shen = view.findViewById<AppCompatRadioButton>(R.id.shen_segment)
-        val mao_hydro = view.findViewById<AppCompatRadioButton>(R.id.mao_hydro_segment)
-        val mao_nHydro = view.findViewById<AppCompatRadioButton>(R.id.mao_nHydro_segment)
+        view.findViewById<AppCompatRadioButton>(R.id.shen_segment).setOnClickListener(this)
+        view.findViewById<AppCompatRadioButton>(R.id.mao_hydro_segment).setOnClickListener(this)
+        view.findViewById<AppCompatRadioButton>(R.id.mao_nHydro_segment).setOnClickListener(this)
+        }
 
-        /*fun onSegmentClicked(view: View) {
-            if (view is AppCompatRadioButton) {
-                val checked = view.isChecked
-                when (view.getId()) {
-                    R.id.shen_segment -> if (checked) {
-                        println("Shen chosen")
-                    }
-                    R.id.mao_hydro_segment -> if (checked) {
-                        println("Mao hydro chosen")
-                    }
-                    R.id.mao_nHydro_segment -> if (checked) {
-                        println("Mao non-hydro chosen")
-                    }
-                }
-            }*/
-
-
-    }
-
-    public fun onSegmentClicked(view: View) {
-        if (view is AppCompatRadioButton) {
-            val checked = view.isChecked
-            when (view.getId()) {
-                R.id.shen_segment -> if (checked) {println("Shen chosen")}
-                R.id.mao_hydro_segment -> if (checked) {println("Mao hydro chosen")}
-                R.id.mao_nHydro_segment -> if (checked) {println("Mao non-hydro chosen")}
-            }
+    override fun onClick(view: View?) {
+        Log.i(ContentValues.TAG, "onClick: view is null: ${view == null}, view.id = ${view?.id}")
+        when (view?.id) {
+            R.id.shen_segment -> Log.i(ContentValues.TAG, "onClick: shen_segment")
+            R.id.mao_hydro_segment -> Log.i(ContentValues.TAG, "onClick: mao_hydro_segment")
+            R.id.mao_nHydro_segment -> Log.i(ContentValues.TAG, "onClick: mao_nHydro_segment")
         }
     }
 }
+
+
